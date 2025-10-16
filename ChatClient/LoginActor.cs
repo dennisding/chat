6cb99085b87ee7c9be3b6f2b1d65ee7421f62812
dennisding <evilangel2000@163.com>
2025-10-16 20:02:17 +0800

    整理客户端的Actor代码

--- ChatClient/LoginActor.cs
+++ ChatClient/LoginActor.cs
@@ -1,80 +1,15 @@
 ﻿
 
-using Common;
 using Protocol;
 using Client;
-using System.Runtime.InteropServices.Swift;
-using Services;
-using System.Net.Sockets;
 
 namespace ChatClient
 {
-    class ActorSender : ISender
+    class LoginActor : ActorClient<ILoginClient, ILoginCore>, ILoginClient
     {
-        ActorId aid;
-        ActorServices services;
-
-        public ActorSender(ActorId aid, ActorServices services)
-        {
-            this.aid = aid;
-            this.services = services;
-        }
-
-        public void Send(MemoryStream stream)
-        {
-            services.remote!.ActorMessage(aid, stream);
-        }
-
-        public void Close()
-        {
-            services.client!.Close();
-        }
-    }
-
-    class LoginActor : Actor, ILoginClient // ActorClient<ILoginCore>
-    {
-        ILoginCore? core;
         public LoginActor()
         {
         }
-
-        public override void Init()
-        {
-        }
-
-        public override void Finit()
-        {
-        }
-
-        public override void EnterWorld()
-        {
-        }
-
-        public override void LeaveWorld()
-        {
-        }
-
-        public override void BindClient(ActorServices? services)
-        {
-            base.BindClient(services);
-
-            if (services == null)
-            {
-                core = null;
-                return;
-            }
-
-            ISender sender = new ActorSender(aid, services);
-            core = new Protocol.Sender.ILoginCore_Sender(sender);
-        }
-
-        public override void DispatchMessage(BinaryReader reader)
-        {
-            //            Protocol.Dispatcher.ILoginClient.Dispatch(this, reader);
-            Protocol.Dispatcher.ILoginClient_Dispatcher.Dispatch(this, reader);
-        }
-
-        // impl ILoginClient
         public void LoginResult(bool isOk)
         {
             Console.WriteLine($"LoginActor.LoginResult: {isOk}");
--- client/Actor.cs
+++ client/Actor.cs
@@ -1,5 +1,4 @@
 ﻿using Common;
-using Services;
 
 namespace Client
 {
@@ -36,6 +35,59 @@ namespace Client
         public virtual void DispatchMessage(BinaryReader reader)
         {
 
+        }
+    }
+
+    public class ActorClient<ClientImpl, ServerImpl> : Actor
+        where ServerImpl: class
+        where ClientImpl : class
+    {
+        public ServerImpl? core;
+        IDispatcher<ClientImpl> dispatcher = Protocol.Dispatcher.Dispatcher.Create<ClientImpl>();
+
+        public ActorClient()
+        {
+            return;
+        }
+
+        public override void BindClient(ActorServices? services) 
+        {
+            base.BindClient(services);
+
+            if (services == null)
+            {
+                core = null;
+                return;
+            }
+
+            ISender sender = new ActorSender(aid, services);
+            core = Protocol.Sender.Sender.Create<ServerImpl>(sender);
+        }
+
+        public override void DispatchMessage(BinaryReader reader)
+        {
+            dispatcher.Dispatch((this as ClientImpl)!, reader);
+        }
+    }
+
+    class ActorSender : ISender
+    {
+        ActorServices services;
+        ActorId aid;
+        public ActorSender(ActorId aid, ActorServices services)
+        {
+            this.aid = aid;
+            this.services = services;
+        }
+
+        public void Send(MemoryStream stream)
+        {
+            services.remote!.ActorMessage(aid, stream);
+        }
+
+        public void Close()
+        {
+            services.client!.Close();
         }
     }
 }
